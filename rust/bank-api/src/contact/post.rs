e7e9e5edd84e75a99ba89854e39be7683589e808
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-07-13 23:12:03 -0400

    Cleaned Up Contact Post Logic

--- rust/bank-api/src/contact/post.rs
+++ rust/bank-api/src/contact/post.rs
@@ -4,45 +4,47 @@ use crate::contact::contact::{Contact, ContactResponse};
 
 //SQL POST LOGIC
 pub(super) async fn add(contact_id: &str, user_id: &str, pool: &Pool<Postgres>) -> Result<ContactResponse, sqlx::Error>{
-    Ok(ContactResponse{
-        contacts: Some(query_as::<_, Contact>(r#"
+    const QUERY: &str = r#"
             INSERT INTO contacts (user_id, contact_id, is_fav)
             VALUES ($1, $2, FALSE) 
             ON CONFLICT (user_id, contact_id) 
             DO NOTHING 
             RETURNING user_id, contact_id, is_fav, contact_name
-        "#).bind(user_id).bind(contact_id).fetch_all(pool).await?),
-    })
+        "#;
+    internal_query(contact_id, user_id, pool, QUERY).await
 }
 pub(super) async fn remove(contact_id: &str, user_id: &str, pool: &Pool<Postgres>) -> Result<ContactResponse, sqlx::Error>{
-    Ok(ContactResponse{
-        contacts: Some(query_as::<_, Contact>(r#"
+    const QUERY: &str = r#"
             DELETE FROM contacts
             WHERE user_id = $1 AND contact_id = $2
             RETURNING user_id, contact_id, is_fav, contact_name
-        "#).bind(user_id).bind(contact_id).fetch_all(pool).await?),
-    })
+        "#;
+    internal_query(contact_id, user_id, pool, QUERY).await
 }
 pub(super) async fn fav(contact_id: &str, user_id: &str, pool: &Pool<Postgres>) -> Result<ContactResponse, sqlx::Error>{
-    Ok(ContactResponse{
-        contacts: Some(query_as::<_, Contact>(r#"
+    const QUERY: &str = r#"
             INSERT INTO contacts (user_id, contact_id, is_fav)
             VALUES ($1, $2, TRUE)
             ON CONFLICT (user_id, contact_id)
             DO UPDATE
                 SET is_fav = TRUE
             RETURNING user_id, contact_id, is_fav, contact_name
-        "#).bind(user_id).bind(contact_id).fetch_all(pool).await?),
-    })
+        "#;
+    internal_query(contact_id, user_id, pool, QUERY).await
+    
 }
 pub(super) async fn un_fav(contact_id: &str, user_id: &str, pool: &Pool<Postgres>) -> Result<ContactResponse, sqlx::Error>{
-    Ok(ContactResponse{
-        contacts: Some(query_as::<_, Contact>(r#"
+    const QUERY: &str = r#"
             UPDATE contacts
             SET is_fav = FALSE
             WHERE user_id = $1 AND contact_id = $2
             RETURNING user_id, contact_id, is_fav, contact_name
-        "#).bind(user_id).bind(contact_id).fetch_all(pool).await?),
-    })
+        "#;
+    internal_query(contact_id, user_id, pool, QUERY).await
 }
 
+async fn internal_query(contact_id: &str, user_id: &str, pool: &Pool<Postgres>, query: &str)-> Result<ContactResponse, sqlx::Error>{
+    Ok(ContactResponse{
+        contacts: Some(query_as::<_, Contact>(query).bind(user_id).bind(contact_id).fetch_all(pool).await?),
+    })
+}
